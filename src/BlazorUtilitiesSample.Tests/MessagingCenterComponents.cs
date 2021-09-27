00671d13da5ad3418cf7fafb551463d9914715f9
Ahmad Mozaffar <aksoftware98@hotmail.com>
2021-09-28 01:35:23 +0300

    Create the xUnit project and setup bUnit for unit testing

---
+++ src/BlazorUtilitiesSample.Tests/MessagingCenterComponents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using BlazorUtilitiesSample.Shared;
+using Bunit;
+using Xunit;
+
+namespace BlazorUtilitiesSample.Tests
+{
+    public class MessagingCenterComponents
+    {
+        public MessagingCenterComponents()
+        {
+        }
+
+        [Fact]
+        public void UpdateUsername_ShouldUpdateNavMenuUsername()
+        {
+            var testContext = new TestContext();
+            var navMenuComponent = testContext.RenderComponent<NavMenu>();
+            var firstAnchor = navMenuComponent.FindAll("a")[1];
+            firstAnchor.MarkupMatches("<span class=\"oi oi - person\" aria-hidden=\"true\" b-g4ljmjz4uk=\"\"></span> Ahmad Mozaffar");
+        }
+
+    }
+}
